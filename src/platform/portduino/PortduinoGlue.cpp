137f4f8a709f6237a28a324ac3436c317e15e478
Nestpebble <waveoctave@protonmail.com>
2025-02-01 03:10:18 +0000

    First go at gettting E80 into yaml...

--- src/platform/portduino/PortduinoGlue.cpp
+++ src/platform/portduino/PortduinoGlue.cpp
@@ -430,7 +430,35 @@ bool loadConfig(const char *configPath)
                     }
                 }
             }
-        }
+            if (settingsStrings[lr11x0_rf_switch] == "E80") {
+                // if its an E80 attached, use this switching table
+                const uint32_t rfswitch_dio_pins[] = {RADIOLIB_LR11X0_DIO5, RADIOLIB_LR11X0_DIO6, RADIOLIB_LR11X0_DIO7,
+                                                    RADIOLIB_NC, RADIOLIB_NC}
+                const Module::RfSwitchMode_t rfswitch_table[] = {
+                    // mode                  DIO5  DIO6  DIO7
+                    {LR11x0::MODE_STBY, {LOW, LOW, LOW}},
+                    {LR11x0::MODE_RX, {LOW, HIGH, LOW}},
+                    {LR11x0::MODE_TX, {HIGH, HIGH, LOW}},
+                    {LR11x0::MODE_TX_HP, {LOW, HIGH, LOW}},
+                    {LR11x0::MODE_TX_HF, {LOW, LOW, LOW}},
+                    {LR11x0::MODE_GNSS, {LOW, LOW, HIGH}},
+                    {LR11x0::MODE_WIFI, {LOW, LOW, LOW}},  END_OF_MODE_TABLE,
+                }
+            } else {
+                // default to Semtech reference design
+                const uint32_t rfswitch_dio_pins[] = {RADIOLIB_LR11X0_DIO5, RADIOLIB_LR11X0_DIO6, RADIOLIB_LR11X0_DIO7,
+                                                    RADIOLIB_NC, RADIOLIB_NC}
+                const Module::RfSwitchMode_t rfswitch_table[] = {
+                    // mode                  DIO5  DIO6  DIO7
+                    {LR11x0::MODE_STBY, {LOW, LOW, LOW}},
+                    {LR11x0::MODE_RX, {HIGH, LOW, LOW}},
+                    {LR11x0::MODE_TX, {HIGH, HIGH, LOW}},
+                    {LR11x0::MODE_TX_HP, {LOW, HIGH, LOW}},
+                    {LR11x0::MODE_TX_HF, {LOW, LOW, LOW}},
+                    {LR11x0::MODE_GNSS, {LOW, LOW, HIGH}},
+                    {LR11x0::MODE_WIFI, {LOW, LOW, LOW}},  END_OF_MODE_TABLE,
+                }
+            }
         if (yamlConfig["GPIO"]) {
             settingsMap[user] = yamlConfig["GPIO"]["User"].as<int>(RADIOLIB_NC);
         }
